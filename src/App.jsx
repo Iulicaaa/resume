8e6fdd1116ada802fa9db1be79c013ec5846f0fd
Iulicaaa <121517898+Iulicaaa@users.noreply.github.com>
2024-01-06 16:29:30 +0000

    Implement page columns

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import Layout from './components/Layout';
 
 export default function App() {
   return (
-    <Layout>
-     alt text
-    </Layout>
+    <Layout
+      left = { 
+        <div>
+          Left side
+        </div>
+      }     
+       right = {
+         <div>
+           Right side
+         </div>
+       } 
+    />
   )
 }
 
