33060709297b1b59fd0d942a5522db5961d69c65
Iulia Halmaghi <iulia.halmaghi@gmail.com>
2024-01-07 17:47:48 +0200

    Fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import './normalize.css';
-import Layout from './components/Layout';
-import Details from './components/Details';
-import WorkEperience from './components/WorkExperience';
-import Hobby from './components/Hobby';
+import "./normalize.css";
+import Layout from "./components/Layout";
+import Details from "./components/Details";
+import WorkExperience from "./components/WorkExperience";
+import Hobby from "./components/Hobby";
 
 export default function App() {
   return (
     <Layout
-      left = { 
+      left={
         <div>
           <Details />
-          <WorkEperience />
-          <Hobby/>
+          <WorkExperience />
+          <Hobby />
         </div>
-      }     
-       right = {
-         <div>
-           Right side
-         </div>
-       } 
+      }
+      right={<div>Right side</div>}
     />
-  )
+  );
 }
-
